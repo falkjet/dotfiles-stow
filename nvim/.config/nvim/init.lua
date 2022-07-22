d7f3adc9ccc577ca68b69197416faa484e482e06
Falk Markus Dursun Jetlund <falk@jetlund.com>
2022-07-22 18:32:31 +0200

    Add neovim config

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,13 @@
+vim.g.mapleader = ' '
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.mouse = 'a'
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+vim.o.textwidth = 80
+vim.opt.formatoptions:remove({'t'})
+vim.o.expandtab = true
+vim.o.updatetime = 300
+vim.o.signcolumn = 'number'
+vim.o.timeoutlen = 300
