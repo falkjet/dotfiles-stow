9fa9cf1f6fc4437375fc2916d236083868ed5ac2
Falk Markus Dursun Jetlund <falk@jetlund.com>
2023-07-18 14:15:14 +0200

    Only start tmux with systemd-run when using systemd

--- tmux/.profile.d/tmux.sh
+++ tmux/.profile.d/tmux.sh
@@ -1,1 +1,3 @@
-alias tmux='systemd-run --scope --user tmux -2 -f "$XDG_CONFIG_HOME/tmux/tmux.conf"'
+if [ "$(ps -p1 -o comm=)" = systemd ]; then
+    alias tmux='systemd-run --scope --user tmux -2 -f "$XDG_CONFIG_HOME/tmux/tmux.conf"'
+fi
