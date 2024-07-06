8d6a576e403a116ff40dad09b4b66a2eee981d63
Falk Markus Dursun Jetlund <falk@jetlund.com>
2024-07-06 23:51:23 +0200

    Hopefully make the setupscript work

--- setupscript/setup.sh
+++ setupscript/setup.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+for command in stow git bc make; do
+    if ! command -v "$command" >/dev/null; then
+        echo command "$command" not found
+        exit 1
+    fi
+done
+
 DOTFILES_REPO="${DOTFILES_REPO:-github.com/falkjet/dotfiles-stow}"
 DOTFILES_REPO_SSH_USER="${DOTFILES_REPO_SSH_USER:-git}"
 DOTFILES_DIRECTORY="${DOTFILES_DIRECTORY:-"$HOME/dotfiles"}"
@@ -13,6 +20,9 @@ if ! [ -e "$DOTFILES_DIRECTORY" ]; then
             exit 1
         fi
     fi
+    if ! [ -z ${DOTFILES_BRANCH:+1} ]; then
+        (cd "$DOTFILES_DIRECTORY" && git checkout "$DOTFILES_BRANCH")
+    fi
 fi
 
 cd "$DOTFILES_DIRECTORY" || (echo failed to change directory to dotfiles directory; exit 1)
@@ -23,15 +33,14 @@ for package in ./*; do
     fi
 done
 
-# stow essential packages
-stow -d "$DOTFILES_DIRECTORY" -t "$HOME" shell setupscript
+
+# Hack unstow packages, so we don't backup symlinks we don't need
+stow -d "$DOTFILES_DIRECTORY" -t "$HOME" -D shell setupscript
 
 for file in ~/.bashrc ~/.zshrc ~/.profile ~/.profile.d ~/.zshrc.d; do
-    # TODO: check if file is symlink to DOTFILES_DIRECTORY
-    continue
-    echo "moving $file to backup"
     if [ -e "$file" ]; then
-        newfile="$1.bak"
+        echo "moving $file to backup"
+        newfile="$file.bak"
         if [ -e "$newfile" ]; then
             n=1
             while [ -e "$newfile$n" ]; do
@@ -39,15 +48,21 @@ for file in ~/.bashrc ~/.zshrc ~/.profile ~/.profile.d ~/.zshrc.d; do
             done
             newfile="$newfile$n"
         fi
+        echo mv -- "$file" "$newfile"
         mv -- "$file" "$newfile"
         echo "$file"
     fi
+done
+
+# stow essential packages
+stow -d "$DOTFILES_DIRECTORY" -t "$HOME" shell setupscript
+
+
+find ~/.setup.d/ -maxdepth 1 -type f,l -print | while read -r file; do
+    "$file"
 done
 
 # source .profile
 # shellcheck disable=SC1090
 . ~/.profile
 
-find ~/.setup.d/ -maxdepth 1 -type f,l -print | while read -r file; do
-    "$file"
-done
