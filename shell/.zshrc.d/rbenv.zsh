9c74571d0658450e713fd76d37b11f1a62b50622
Falk Markus Dursun Jetlund <falk@jetlund.com>
2026-02-10 13:42:47 +0100

    Move rbev to shell config

---
+++ shell/.zshrc.d/rbenv.zsh
@@ -0,0 +1,20 @@
+command -v rbenv >/dev/null || exit
+
+export RBENV_ROOT="$HOME/.local/share/rbenv"
+export PATH="$RBENV_ROOT/shims:${PATH}"
+export RBENV_SHELL=zsh
+
+rbenv() {
+  local command
+  command="${1:-}"
+  if [ "$#" -gt 0 ]; then
+    shift
+  fi
+
+  case "$command" in
+  rehash|shell)
+    eval "$(rbenv "sh-$command" "$@")";;
+  *)
+    command rbenv "$command" "$@";;
+  esac
+}
